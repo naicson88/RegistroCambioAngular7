16a95964dd10effa34df156c81aa57e6b85b3dc4
Alan Naicson Costa Cunha <alan.cunha@AD002.NET02>
2020-03-17 11:46:14 -0300

    Primeiro commit

---
+++ src/app/shared/employee.model.ts
@@ -0,0 +1,16 @@
+export class Employee {
+    id : string;
+    dtVenc : string;
+    fullName: string;
+    vlDolar : string;
+    parcela: string;
+    cotacao : string;
+    vlReal : string;
+    dtFecha: string;
+    banco: string;
+    contrato: string;
+    modal : string;
+    nBlHawb : string;
+
+}
+
